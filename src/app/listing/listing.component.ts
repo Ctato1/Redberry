ce306ed0f4005e957dafddf168524c8ec45f502a
Ctato1 <dznelashvilitato@gmail.com>
2024-09-13 05:25:09 +0400

    add: region and city selection in reactive forms

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -1,43 +1,69 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import {CitiesProps, FilterService, RegionProps} from "../shared/filter.service";
 
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
   styleUrl: './listing.component.css'
 })
-export class ListingComponent implements OnInit{
-  types:string[] = ['იყიდება', 'ქირავდება'];
 
-  listingForm!:FormGroup;
-  options1 = [
-    { label: 'Option 1', value: 1 },
-    { label: 'Option 2', value: 2 },
-    { label: 'Option 3', value: 3 }
-  ];
+
+export class ListingComponent implements OnInit {
+  types: string[] = ['იყიდება', 'ქირავდება'];
+
+  listingForm!: FormGroup;
+
+  regions: RegionProps[] = [];
+  allcities: CitiesProps[] = [];
+  chosenCity: CitiesProps[] = [];
+
+
+  constructor(private filterService: FilterService) {
+  }
 
   ngOnInit() {
+    this.fetchData();
+    this.initForm();
+  }
+
+  initForm() {
     this.listingForm = new FormGroup({
-      "location" : new FormGroup({
-        'address' : new FormControl(null,[Validators.required]),
-        'zip' : new FormControl(null,[Validators.required]),
-        'region' : new FormControl(null,[Validators.required]),
-        'city' : new FormControl(null,[Validators.required]),
+      "location": new FormGroup({
+        'address': new FormControl(null, [Validators.required]),
+        'zip': new FormControl(null, [Validators.required]),
+        'region': new FormControl(null, [Validators.required]),
+        'city': new FormControl(null, [Validators.required]),
       }),
-      "details" : new FormGroup({
-        'price' : new FormControl(null,[Validators.required]),
-        'area' : new FormControl(null,[Validators.required]),
-        'bedroom' : new FormControl(null,[Validators.required]),
-        'description' : new FormControl(null,[Validators.required]),
-        'photo' : new FormControl(null,[Validators.required]),
+      "details": new FormGroup({
+        'price': new FormControl(null, [Validators.required]),
+        'area': new FormControl(null, [Validators.required]),
+        'bedroom': new FormControl(null, [Validators.required]),
+        'description': new FormControl(null, [Validators.required]),
+        'photo': new FormControl(null, [Validators.required]),
       }),
-      'types' : new FormControl('იყიდება'),
-      'agent' : new FormControl(null,[Validators.required]),
+      'types': new FormControl('იყიდება'),
+      'agent': new FormControl(null, [Validators.required]),
+    });
+  }
+
+  fetchData() {
+    // get Regions
+    this.filterService.getRegions().subscribe((regions: RegionProps[]) => {
+      this.regions = regions;
+    })
+    this.filterService.getCities().subscribe((cities: CitiesProps[]) => {
+      this.allcities = cities;
+      console.log(cities)
     })
   }
 
+  changeCities() {
+    const currentRegionId:number = this.listingForm.get('location.region')?.value?.id;
+    this.chosenCity = this.allcities.filter((item:CitiesProps) => item.region_id === currentRegionId);
+  }
 
-  onSubmit(){
-
+  onSubmit() {
+    console.log(this.listingForm)
   }
 }
--- src/app/shared/filter.service.ts
+++ src/app/shared/filter.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {catchError, throwError} from "rxjs";
 
@@ -7,15 +7,27 @@ export interface RegionProps {
   id: number;
   name: string;
 }
+
+export interface CitiesProps {
+  id: number,
+  name: string,
+  region_id: number
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class FilterService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
 
-  getRegions(){
-    return this.http.get<RegionProps[]>('https://api.real-estate-manager.redberryinternship.ge/api/regions').pipe(catchError(errorRes=> throwError(errorRes)))
+  getRegions() {
+    return this.http.get<RegionProps[]>('https://api.real-estate-manager.redberryinternship.ge/api/regions').pipe(catchError(errorRes => throwError(errorRes)))
+  }
+
+  getCities() {
+    return this.http.get<CitiesProps[]>('https://api.real-estate-manager.redberryinternship.ge/api/cities').pipe(catchError(errorRes => throwError(errorRes)))
   }
 }
